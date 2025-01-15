2bfaae9cb165f3177ca36e007355640b6a29d26a
anshggss <ptmtripathi@gmail.com>
2025-01-15 22:29:44 +0530

    NavBar improved

--- src/navBar/navBar.jsx
+++ src/navBar/navBar.jsx
@@ -1,20 +1,35 @@
 import React from "react";
 import "./navBar.css";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const navBar = () => {
+  const location = useLocation();
+
   return (
     <div className="navBar">
       <div className="navLeft">
-        <Link to="/" className="home">
+        <Link
+          to="/"
+          className={`home ${location.pathname === "/" ? "activeNav" : ""} `}
+        >
           Home
         </Link>
       </div>
       <div className="navRight">
-        <Link to="/favorites" className="favorites">
+        <Link
+          to="/favorites"
+          className={`favorites ${
+            location.pathname === "/favorites" ? "activeNav" : ""
+          }`}
+        >
           Favorites
         </Link>
-        <Link to="/watch-now" className="watchNow">
+        <Link
+          to="/watch-now"
+          className={`watchNow ${
+            location.pathname === "/watch-now" ? "activeNav" : ""
+          }`}
+        >
           Watch Now
         </Link>
       </div>
